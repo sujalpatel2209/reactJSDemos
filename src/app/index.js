bf72a4da20b1bafb8daa241de4515fde29f93ee7
sujalpatel2209 <sujalpatel022@gmail.com>
2017-07-09 15:18:01 +0530

    create multiple component and call in index.js file

--- src/app/index.js
+++ src/app/index.js
@@ -3,13 +3,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
 class App extends React.Component {
     render() {
         return(
             <div className="container">
                 <div className='row'>
                     <div className="col-xs-10 col-xs-offset-1">
-                        <h1>Hello Sujal</h1>
+                        <Header/>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home/>
                     </div>
                 </div>
             </div>
