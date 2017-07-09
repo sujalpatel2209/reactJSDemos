d7393d761333a43f94acc9693acddf6597788fcb
sujalpatel2209 <sujalpatel022@gmail.com>
2017-07-09 15:36:09 +0530

    display dynamic output

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 export class Home extends React.Component{
     render(){
+          let content = "";
+            if(true){
+                content = <p>Hello It's true</p>
+            }
         return(
             <div>
                 <p>This is new Component</p>
+                { content }{ 5 + 5 }
             </div>
         );
     }
