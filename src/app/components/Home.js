272d11639046258e53a84e435dd7b87ddf03aab1
sujalpatel2209 <sujalpatel022@gmail.com>
2017-07-13 22:48:49 +0530

    check render the component change part

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-export class Home extends React.Component{
+export class Home extends React.Component {
 
     constructor(props) {
         super();
         this.state = {
-            age: props.initialAge
-        }
+            age: props.initialAge,
+            status : 0
+        };
+        setTimeout(() => {
+            this.setState({
+                status : 1
+            });
+        },3000);
     }
 
     increaseAge() {
@@ -15,18 +21,19 @@ export class Home extends React.Component{
         });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <p>This is new Component</p>
                 <p>Name : {this.props.name}</p>
                 <p>Age : {this.state.age}</p>
+                <p>Status : {this.state.status}</p>
                 <p>User Name : {this.props.user.name}</p>
                 <h2>Skills</h2>
                 <ul>
-                    {this.props.user.skills.map((skills,i) => <li key={i}>{skills}</li>)}
+                    {this.props.user.skills.map((skills, i) => <li key={i}>{skills}</li>)}
                 </ul>
-                <hr/>
+                <hr />
                 {/*<button onClick={this.increaseAge.bind(this)} className="btn btn-primary">Increase Age</button>*/}
                 <button onClick={() => this.increaseAge()} className="btn btn-primary">Increase Age</button>
             </div>
