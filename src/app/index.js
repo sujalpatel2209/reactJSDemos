14a6a909d934203e9b20a359f4e00ea81456eb06
sujalpatel2209 <sujalpatel022@gmail.com>
2017-07-13 23:03:56 +0530

    Create stateless components

--- src/app/index.js
+++ src/app/index.js
@@ -16,7 +16,7 @@ class App extends React.Component {
             <div className="container">
                 <div className='row'>
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
+                        <Header home="Home"/>
                     </div>
                 </div>
                 <div className='row'>
